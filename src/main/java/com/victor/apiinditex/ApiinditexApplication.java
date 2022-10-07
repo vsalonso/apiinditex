25d0a49ccf81343658e654efbf6271df00619398
victor.soto <victor.soto@avoristravel.com>
2022-10-07 18:14:58 +0200

    Initial commit

---
+++ src/main/java/com/victor/apiinditex/ApiinditexApplication.java
@@ -0,0 +1,13 @@
+package com.victor.apiinditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiinditexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiinditexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victor/apiinditex/ApiinditexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.apiinditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiinditexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
